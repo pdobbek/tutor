764c93e3546cd7e2f07d22d8cdc3d3615eff0a43
Patryk <p.dobbek@pm.me>
2024-08-27 19:21:42 +0200

    feat: add recaptcha

--- tutor.py
+++ tutor.py
@@ -1,11 +1,12 @@
 from dotenv import dotenv_values
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mail import Mail, Message
 
-
-app = Flask(__name__)
+import verify_recaptcha
 
 config = dotenv_values(".env")
+app = Flask(__name__)
+
 app.config["MAIL_SERVER"] = config["MAIL_SERVER"]
 app.config["MAIL_PORT"] = int(config["MAIL_PORT"])
 app.config["MAIL_USERNAME"] = config["MAIL_USERNAME"]
@@ -16,10 +17,33 @@ mail = Mail(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    sitekey = config['RECAPTCHA_SITE_KEY']
+    return render_template('index.html', sitekey=sitekey)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    # get token from recaptcha
+    recaptcha_token = request.form["g-recaptcha-response"]
+    recaptcha_score = verify_recaptcha.get_assessment_score(recaptcha_token, "SUBMIT")
+    if recaptcha_score < 0.5:
+        return "Recaptcha failed. If you're not a bot, try again."
+    # get data from form
+    name = request.form["name"]
+    email = request.form["email"]
+    message = request.form["message"]
+    # send email
+    msg = Message(
+        subject='Tutor Request', 
+        sender=config['MAIL_USERNAME'],
+        recipients=[config['MAIL_USERNAME']]
+    )
+    msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+    mail.send(msg)
+    return "Message sent!"
+
 
 @app.route("/sendemail")
-def index():
+def send_email():
     msg = Message(
         subject='Hello from the other side!', 
         sender=config['MAIL_USERNAME'],
